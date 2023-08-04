6f4524eb830eae521a593a993b78de95d2d30bee
Aaron He <aaronhe09@yahoo.com>
2023-08-04 01:20:53 +0000

    rendered title of each faction group and name of each faction within those groups

--- client/src/Components/Factions/Factions.js
+++ client/src/Components/Factions/Factions.js
@@ -1,7 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { readAllFactions } from '../../data';
 
 export default function Factions() {
+  const [groupedFactions, setGroupedFactions] = useState([]);
+
   useEffect(() => {
     async function fetchAllFactions() {
       try {
@@ -10,12 +12,41 @@ export default function Factions() {
           obj[cur.factionGroup] = obj[cur.factionGroup] || [];
           obj[cur.factionGroup].push(cur);
           return obj;
-        });
-        console.log(groupBy);
+        }, {});
+        setGroupedFactions(groupBy);
       } catch (err) {
         console.error(err);
       }
     }
     fetchAllFactions();
   }, []);
+
+  return (
+    <main>
+      <h1>Factions</h1>
+      <RenderFactions groupedFactions={groupedFactions} />
+    </main>
+  );
+}
+
+function RenderFactions({ groupedFactions }) {
+  const keys = Object.keys(groupedFactions);
+
+  return (
+    <div>
+      {keys.map((key) => {
+        const array = groupedFactions[key];
+        return (
+          <section id="faction-group-container" key={key}>
+            <h1>{key}</h1>
+            <ul className="row">
+              {array.map((faction) => (
+                <li key={faction.factionId}>{faction.factionName}</li>
+              ))}
+            </ul>
+          </section>
+        );
+      })}
+    </div>
+  );
 }
