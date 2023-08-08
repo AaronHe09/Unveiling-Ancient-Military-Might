c4b5368c3af016b7a622fa2235dd0ac97306980b
Aaron He <aaronhe09@yahoo.com>
2023-08-08 08:25:35 +0000

    added login page and styled it;

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,5 +1,24 @@
 import './Login.css';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
-  return <div>login</div>;
+  return (
+    <main>
+      <form id="login">
+        <fieldset>
+          <legend>Login</legend>
+          <div>
+            <input type="text" placeholder="Username" required />
+          </div>
+          <div>
+            <input type="text" placeholder="Password" required />
+          </div>
+          <button>Login</button>
+          <p>
+            Don't have an account? <Link>Sign Up</Link>
+          </p>
+        </fieldset>
+      </form>
+    </main>
+  );
 }
