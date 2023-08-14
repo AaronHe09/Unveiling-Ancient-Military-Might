6bdbad30dd6c5b909139a2fb8a7dfe6cdb1bf513
Aaron He <aaronhe09@yahoo.com>
2023-08-14 14:45:17 +0000

    setup layout for Battlefield

--- client/src/pages/Battlefield/Battlefield.js
+++ client/src/pages/Battlefield/Battlefield.js
@@ -1,9 +1,44 @@
 import './Battlefield.css';
+import { useEffect, useState } from 'react';
+import { readUserGenerals, readUserUnits } from '../../data';
 
 export default function Battlefield() {
+  const [userUnits, setUserUnits] = useState([]);
+  const [userGenerals, setUserGenerals] = useState([]);
+
+  useEffect(() => {
+    async function fetchUserData() {
+      try {
+        const unitsData = await readUserUnits();
+        const generalsData = await readUserGenerals();
+        const groupBy = unitsData.reduce((obj, cur) => {
+          obj[cur.unitType] = obj[cur.unitType] || [];
+          obj[cur.unitType].push(cur);
+          return obj;
+        }, {});
+        console.log(generalsData);
+        setUserGenerals(generalsData);
+        setUserUnits(groupBy);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    fetchUserData();
+  }, []);
+
   return (
     <main>
-      <div id="battlefield-container"></div>
+      <div id="battlefield-container">
+        <div className="row">
+          <div className="column-half">
+            <div className="infantry"></div>
+            <div className="missile"></div>
+          </div>
+          <div className="column-half">
+            <div className="cavalry"></div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
