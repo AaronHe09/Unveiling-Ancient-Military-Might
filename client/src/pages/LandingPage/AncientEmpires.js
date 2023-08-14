fbf66b8a12d972b24ae2cf6f426df5cca32136b8
Aaron He <aaronhe09@yahoo.com>
2023-08-14 18:07:19 +0000

    wrapped links to AncientEmpires pictures

--- client/src/pages/LandingPage/AncientEmpires.js
+++ client/src/pages/LandingPage/AncientEmpires.js
@@ -3,6 +3,7 @@ import eygpt from '../../images/egypt.png';
 import rome from '../../images/rome.png';
 import macedonia from '../../images/macedonia.png';
 import seleucid from '../../images/seleucid.png';
+import { Link } from 'react-router-dom';
 
 export default function AncientEmpires() {
   return (
@@ -11,32 +12,40 @@ export default function AncientEmpires() {
         <h1>Ancient Empires</h1>
         <div className="column-full">
           <div className="row">
-            <div className="column-half">
-              <div className="img-wrapper">
-                <img src={eygpt} alt="Egyptian person" />
+            <Link to="/factions/67">
+              <div className="column-half">
+                <div className="img-wrapper">
+                  <img src={eygpt} alt="Egyptian person" />
+                </div>
+                <h3>Egyptian Empire</h3>
               </div>
-              <h3>Egyptian Empire</h3>
-            </div>
-            <div className="column-half">
-              <div className="img-wrapper">
-                <img src={rome} alt="Ruined Romanin colosseum" />
+            </Link>
+            <Link to="factions/17">
+              <div className="column-half">
+                <div className="img-wrapper">
+                  <img src={rome} alt="Ruined Romanin colosseum" />
+                </div>
+                <h3>Roman Empire</h3>
               </div>
-              <h3>Roman Empire</h3>
-            </div>
+            </Link>
           </div>
           <div className="row">
-            <div className="column-half">
-              <div className="img-wrapper">
-                <img src={macedonia} alt="Egyptian person" />
+            <Link to="factions/66">
+              <div className="column-half">
+                <div className="img-wrapper">
+                  <img src={macedonia} alt="Egyptian person" />
+                </div>
+                <h3>Macedonian Empire</h3>
               </div>
-              <h3>Macedonian Empire</h3>
-            </div>
-            <div className="column-half">
-              <div className="img-wrapper">
-                <img src={seleucid} alt="Ruined Romanin colosseum" />
+            </Link>
+            <Link to="factions/68">
+              <div className="column-half">
+                <div className="img-wrapper">
+                  <img src={seleucid} alt="Ruined Romanin colosseum" />
+                </div>
+                <h3>Seleucid Empire</h3>
               </div>
-              <h3>Seleucid Empire</h3>
-            </div>
+            </Link>
           </div>
         </div>
       </div>
